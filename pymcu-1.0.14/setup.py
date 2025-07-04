76441e61d51e0fbaebdc8bf72279debc9574e39e
PhiFischer <phi.fischer1@gmail.com>
2025-07-04 11:03:51 +0100

    first round of changes to make it work on python3 (3.12)

---
+++ pymcu-1.0.14/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from distutils.core import setup
+
+setup(name='pymcu',
+      version='1.0.14',
+      description='pyMCU - Python Micro Controller Wrapper',
+      author='Richard Wardlow',
+      author_email='richard@pymcu.com',
+      url='http://www.pymcu.com',
+      py_modules=['pymcu'],
+     )
